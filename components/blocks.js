47cdc7dc86e81e03bac1cc5575bb4d04c7149bc2
Ben Smith <ben@wild.as>
2020-12-06 21:35:38 +0100

    Delete unused bits

--- components/blocks.js
+++
@@ -1,4 +0,0 @@
-export const QuoteBlock = ({ content, author }) => <div className="bg-white p-6">
-  <div className="text-2xl">{ content }</div></div>
-
-export const TextBlock = ({ contents }) => <div className="bg-white p-6">{ contents }</div>

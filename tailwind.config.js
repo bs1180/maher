27659dfe7f2c7ba311799c5695ad62697552cf51
Ben Smith <ben@wild.as>
2020-12-06 21:36:18 +0100

    Update libs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
   purge: [],
-  theme: {
-    extend: {},
-  },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }

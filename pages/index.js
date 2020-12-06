c3ff826b3ea3b32322be0d6b1079437392f3cc4e
Ben Smith <ben@wild.as>
2020-12-06 21:36:38 +0100

    Improve design

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,11 @@ import { useWindowScroll } from "react-use";
 import { useWindowSize } from "react-use";
 import mapboxgl from "mapbox-gl";
 import { QuoteBlock, TextBlock } from "../components/blocks";
+import { getPage } from "../lib/api";
 
 import { createBoxes, createLine, getArc, wrap } from "../components/data";
 
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
 const origin = {
   lat: 48.14652,
@@ -20,7 +21,7 @@ const dest = {
   lat: 24.42479,
 };
 
-export default function Home({ blocks = [], arc = [] }) {
+export default function Home({ blocks = [], arc = [], ...props }) {
   const mapRef = useRef();
   const mapWrapperRef = useRef();
   const [blockRefs, setBlockRefs] = useState([]);
@@ -39,35 +40,37 @@ export default function Home({ blocks = [], arc = [] }) {
   const xRef = useRef({ step: 0 });
 
   const handleUpdate = () => {
-    if (xRef.current.step && mapRef.current && mapRef.current.getSource("route")) {
+    if (
+      xRef.current.step &&
+      mapRef.current &&
+      mapRef.current.getSource("route")
+    ) {
       const partialArc = arc.slice(0, xRef.current.step);
       mapRef.current.getSource("route").setData(wrap(partialArc));
     }
   };
 
   useEffect(() => {
-    if (blockRefs.length) {
-      gsap.to(xRef.current, {
-        step: 499,
-        snap: "step",
-        scrollTrigger: {
-          trigger: mapWrapperRef.current,
-          start: "top top",
-          endTrigger: blockRefs[blockLength - 1].current,
-          end: "center center",
-          scrub: 0.5,
-          pin: true,
-        },
-        onUpdate: handleUpdate,
-      });
-    }
-  }, [mapWrapperRef, blockRefs]);
+    gsap.to(xRef.current, {
+      step: 499,
+      snap: "step",
+      scrollTrigger: {
+        trigger: mapWrapperRef.current,
+        start: "top top",
+        endTrigger: "#last",
+        end: "center center",
+        scrub: 0.5,
+        pin: true,
+      },
+      onUpdate: handleUpdate,
+    });
+  }, [mapWrapperRef]);
 
   useEffect(() => {
     if (false || mapWrapperRef.current) {
       mapRef.current = new mapboxgl.Map({
         container: mapWrapperRef.current,
-        style: "mapbox://styles/mapbox/streets-v11",
+        style: "mapbox://styles/benedictsmith/ckidiozas0sct19quummf8gmd",
         center: [origin.lng, origin.lat],
         draggable: false,
         scrollZoom: false,
@@ -75,7 +78,7 @@ export default function Home({ blocks = [], arc = [] }) {
       });
 
       mapRef.current.on("load", function () {
-        setLoading(false)
+        setLoading(false);
         mapRef.current.addSource("route", {
           type: "geojson",
           data: wrap([0, 0]),
@@ -86,24 +89,30 @@ export default function Home({ blocks = [], arc = [] }) {
           source: "route",
           type: "line",
           paint: {
-            "line-width": 2,
-            "line-color": "#007cbf",
+            "line-width": 3,
+            "line-color": "#000000",
           },
         });
       });
     }
   }, [mapWrapperRef]);
 
-  const renderBlock = ({ _type, ...rest }) => {
-    return _type === "quote" ? <QuoteBlock {...rest} /> : _type === "image" ? null : <TextBlock {...rest} />;
-  };
-
   return (
     <div>
       <Head>
-        <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css" rel="stylesheet" />
+        <title>Walk with us!</title>
+        <link
+          href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
       </Head>
-      <div className={`bg-black inset-0 absolute z-50 text-white ${loading ? "absolute" : "hidden"}`}>loading...</div>
+      <div
+        className={`bg-black inset-0 absolute z-50 text-white ${
+          loading ? "absolute" : "hidden"
+        }`}
+      >
+        loading...
+      </div>
       <div className="">
         <div
           ref={mapWrapperRef}
@@ -111,28 +120,56 @@ export default function Home({ blocks = [], arc = [] }) {
         >
           <div>map goes here </div>
         </div>
-        <div className="z-30 absolute top-0 space-y-64 bg-transparent m-6 max-w-md ">
-          {blocks.map((props, i) => (
-            <div key={i} ref={blockRefs[i]}>
-              {renderBlock(props)}
+        <div className="z-30 absolute top-0 bg-transparent mx-8 my-24 max-w-md space-y-24">
+          {props.topLogo?.url && (
+            <div class="flex justify-center p-4">
+              <img src={props.topLogo?.url} alt="" />
             </div>
-          ))}
+          )}
+
+          <div
+            dangerouslySetInnerHTML={{ __html: props.firstBlock.html }}
+            class="p-8 bg-white opacity-75 prose"
+          />
+                    <div
+            dangerouslySetInnerHTML={{ __html: props.secondBlock.html }}
+            class="p-8 bg-white opacity-75 prose"
+          />
+          <div
+            id="last"
+            class="p-8 p-8 bg-white opacity-75 prose"
+            dangerouslySetInnerHTML={{ __html: props.thirdBlock.html }}
+          />
         </div>
-        <div className="p-32 bg-teal-100">
-          <div>Pledge some money</div>
+        <div className="p-32 bg-black">
+          <div
+            class="max-w-md mx-auto p-6 bg-yellow-50 prose"
+            dangerouslySetInnerHTML={{ __html: props.donationBlock.html }}
+          />
         </div>
-        <footer className="p-32 bg-gray-800">
-          <div>Built by Ben Smith</div>
-        </footer>
       </div>
     </div>
   );
 }
 
 export async function getServerSideProps() {
+  const contents = await getPage("homepage");
+  console.log(contents);
+
+  const origin = {
+    lat: contents.originPointLatitude,
+    lng: contents.originPointLongitude,
+  };
+
+  const dest = {
+    lat: contents.destinationPointLatitude,
+    lng: contents.destinationPointLongitude,
+  };
+
   return {
     props: {
-      blocks: createBoxes(4),
+      ...contents,
+      blocks: [],
       arc: getArc(origin, dest),
     },
   };
